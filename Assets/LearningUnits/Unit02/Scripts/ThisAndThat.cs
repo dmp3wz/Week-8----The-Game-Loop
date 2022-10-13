855f6c2f6b0ff0ef3a5e625f142fe1621574b9f2
dmp3wz <112030592+dmp3wz@users.noreply.github.com>
2022-10-12 22:51:04 -0400

    completed levels

--- Assets/LearningUnits/Unit02/Scripts/ThisAndThat.cs
+++ Assets/LearningUnits/Unit02/Scripts/ThisAndThat.cs
@@ -6,7 +6,7 @@ public class ThisAndThat : MonoBehaviour
 {
     [SerializeField] GameObject blueCube, redCube, door;
 
-    bool isRedCubeDetected, isBlueCubeDeteced, areBothCubesDetected;
+    bool isRedCubeDetected, isBlueCubeDetected, areBothCubesDetected;
 
     // DIRECTIONS: This script is missing a crucial "if statement" in the Update() method.
     // Nothing in Update() needs to change, but something must be added...
@@ -28,22 +28,29 @@ public class ThisAndThat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isBlueCubeDetected && isRedCubeDetected){
+            areBothCubesDetected = true;
+            
+        }
         if (areBothCubesDetected)
         {
-            door.GetComponent<MoveToLocation>().enabled = true;
+                door.GetComponent<MoveToLocation>().enabled = true;
+            
         }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.gameObject something something)
-        //{
-        //    isBlueCubeDeteced = true;
-        //}
+        if (other.gameObject == blueCube)
+        {
+            isBlueCubeDetected = true;
+        }
 
-        //if (other.gameObject something something)
-        //{
-        //    isRedCubeDetected = true;
-        //}
+        if (other.gameObject == redCube)
+        {
+            isRedCubeDetected = true;
+        }
+        
     }
 }
